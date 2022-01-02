fd017ac9ea9d2b73d96c7560a0f2f0088ecf71ed
Christian Puchinger <puchinger@burda.com>
2022-01-02 16:33:03 +0100

    Create ArrayExtension.php

---
+++ Twig/ArrayExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HBM\TwigExtensionsBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class ArrayExtension extends AbstractExtension {
+
+  public function getFilters() : array {
+    return [
+      new TwigFilter('hbmEnumeration', [$this, 'hbmEnumeration']),
+    ];
+  }
+
+  /****************************************************************************/
+  /* FILTERS                                                                  */
+  /****************************************************************************/
+
+  public function hbmEnumeration($vars, $sep = ', ', $sepLast = ' und ', $empty = '') : string {
+    if (\count($vars) === 0) {
+      return $empty;
+    }
+
+    $last = array_pop($vars);
+
+    $string = implode($sep, $vars);
+    if ($string && $last) {
+      $string .= $sepLast;
+    }
+    if ($last) {
+      $string .= $last;
+    }
+
+    return $string;
+  }
+
+}
