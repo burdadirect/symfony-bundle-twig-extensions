0b63234e1c7568f3355eb4df50d613ddc35e64cd
Christian Puchinger <puchinger@burda.com>
2022-01-27 05:23:10 +0100

    Create MailtoExtension.php

---
+++ Twig/MailtoExtension.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace HBM\TwigExtensionsBundle\Twig;
+
+use Twig\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class MailtoExtension extends AbstractExtension {
+
+  /****************************************************************************/
+  /* DEFINITIONS                                                              */
+  /****************************************************************************/
+
+  public function getFilters() : array {
+    return [
+      new TwigFilter('mailto', [$this, 'mailtoFilter']),
+    ];
+  }
+
+  public function getFunctions() : array {
+    return [
+      new TwigFunction('mailto', [$this, 'mailtoFunction']),
+    ];
+  }
+
+  /****************************************************************************/
+  /* HELPER                                                                   */
+  /****************************************************************************/
+
+  private function mailtoEncode(string $var): string {
+    return str_replace("\n", '%0D%0A', htmlentities($var));
+  }
+
+  /****************************************************************************/
+  /* FILTER                                                                   */
+  /****************************************************************************/
+
+  public function mailtoFilter(string $var): string {
+    return $this->mailtoEncode($var);
+  }
+
+  /****************************************************************************/
+  /* FUNCTIONS                                                                */
+  /****************************************************************************/
+
+  public function mailtoFunction(string $emailAddress, ?string $subject, ?string $body): string {
+    $string = 'mailto:'.$emailAddress;
+    $parts = [];
+
+    if ($subject) {
+      $parts[] = 'subject='.$this->mailtoEncode($subject);
+    }
+
+    if ($subject) {
+      $parts[] = 'body='.$this->mailtoEncode($body);
+    }
+
+    if (count($parts) > 0) {
+      $string .= '?';
+    }
+    $string .= implode('&', $parts);
+
+    return $string;
+  }
+
+}
