a98a5966be818f3fd4dbb540c03388c09c3529d4
Christian Puchinger <puchinger@burda.com>
2026-06-11 17:16:05 +0200

    Fix getVarValueParsed.

--- src/SettingContentProvider/SettingContentProviderInterface.php
+++ src/SettingContentProvider/SettingContentProviderInterface.php
@@ -4,5 +4,5 @@ namespace HBM\TwigExtensionsBundle\SettingContentProvider;
 
 interface SettingContentProviderInterface
 {
-    public function getVarValueParsed(string $key, ?string $nature = null, $default = null, $orderBy = null): mixed;
+    public function getVarValueParsed(string $key, ?string $nature = null, mixed $default = null, ?array $orderBy = null): mixed;
 }
