0328723f21ff92a46e2a3f599fbdea431c428866
Christian Puchinger <puchinger@burda.com>
2026-09-15 17:17:32 +0200

    Create CssClassExtension.php

---
+++ src/Twig/Extension/CssClassExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace HBM\TwigExtensionsBundle\Twig\Extension;
+
+use Random\RandomException;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use Twig\TwigTest;
+
+class CssClassExtension extends AbstractExtension
+{
+
+    public function getFilters(): array
+    {
+        return [
+          new TwigFilter('cssClass', $this->cssClass(...)),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+          new TwigFunction('cssClass', $this->cssClass(...)),
+        ];
+    }
+
+    public static function cssClass(string|array|null $classesExisting, string|array|null $classesNew): string
+    {
+        if ($classesExisting === null) {
+            $classesExisting = [];
+        } elseif (is_string($classesExisting)) {
+            $classesExisting = array_map('trim', explode(' ', $classesExisting));
+        }
+
+        if ($classesNew === null) {
+            $classesNew = [];
+        } elseif (is_string($classesNew)) {
+            $classesNew = array_map('trim', explode(' ', $classesNew));
+        }
+
+        $classesReplace = [];
+        $classesRemove = [];
+        foreach ($classesNew as $class) {
+            if (str_starts_with('-', $class)) {
+                $classesRemove[] = substr($class, 1);
+            } elseif (str_starts_with('+', $class)) {
+                $classesExisting[] = substr($class, 1);
+            } else {
+                $classesReplace[] = $class;
+            }
+        }
+
+        if (count($classesReplace) > 0) {
+            return implode(' ', $classesReplace);
+        }
+
+        return implode(' ', array_unique(array_diff($classesExisting, $classesRemove)));
+    }
+}
